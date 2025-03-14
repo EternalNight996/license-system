09b5755ea7a9ae6c67f5893bb1b8cf02f56dbe37
Eternal <eternalnightyeah2@yeah.net>
2025-03-14 11:12:34 +0800

    1

--- src/protocols/mod.rs
+++ src/protocols/mod.rs
@@ -1,6 +1,8 @@
 #[cfg(feature = "chacha20")]
 pub mod chacha20;
-pub use chacha20::ChaCha20Protocol;
 #[cfg(feature = "chacha20")]
+pub use chacha20::ChaCha20Protocol;
+#[cfg(feature = "aes256gcm")]
 pub mod aes256gcm;
+#[cfg(feature = "aes256gcm")]
 pub use aes256gcm::AesGcmProtocol;
